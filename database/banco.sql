c99e6a76f05d80fc4ec7c78a6e428c7d4215643e
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2025-03-17 22:06:04 -0300

    Create banco.sql

---
+++ database/banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dados (
+    AlunoID int PRIMARY KEY AUTO_INCREMENT,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Host varchar(50)
+);
